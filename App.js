8f20f92548ccf9c20df11ef39933c23db6b2437f
daniel corcoran <danielc.92@outlook.com>
2019-11-23 17:23:13 +1100

    Experimented with flexbox

--- App.js
+++ App.js
@@ -1,23 +1,31 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import {
+  StyleSheet, 
+  Text,
+  TextInput,
+  View, 
+  Button } from 'react-native';
 import data from './mock_data';
+/*
+Notes
+- Text can only be placed within <Text> component
+- <View> components represents <divs>, <sections> etc
+
+*/
 
 export default function App() {
 
-  const [outputText, setOutputText] = useState('Open up App.js to start working on app.')
   return (
-    <View style={styles.container}>
-      <Text>{ outputText }</Text>
-      <Button title="Change text" onPress={()=>setOutputText('This is the new text')}></Button>
+    <View style={{padding: 30, display: 'flex', flexDirection: 'row'}}>
+      <View style={{flex: 2, padding: 5}}>
+        <TextInput
+        placeholder="Enter a goal..."
+        style={{ borderBottomColor: 'black', borderBottomWidth: 1, marginBottom: 2}}/>
+        
+      </View>
+      <View stlye={{flex: 1}}>
+         <Button title="Click me"/>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # React Native Test App
 Testing functionality of React Native in windows env.
+
+### Starting the app
+
+`npm start`
+
+Open in expo app on android device or hook into emulator.
+
+### Sources
+
+- [React Native Crash Course by Academind](https://www.youtube.com/watch?v=qSRrxpdMpVc)
