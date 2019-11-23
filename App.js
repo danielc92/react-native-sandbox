3c1f4da0e0561039137888e63d13f5e242fe277d
daniel corcoran <danielc.92@outlook.com>
2019-11-23 20:05:08 +1100

    Added FlatList render

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import {
   StyleSheet, 
+  FlatList,
   Text,
   TextInput,
   View, 
@@ -10,7 +11,7 @@ import data from './mock_data';
 Notes
 - Text can only be placed within <Text> component
 - <View> components represents <divs>, <sections> etc
-
+- Scrolling isnt enabled by default, scrolling is explicit
 */
 
 export default function App() {
@@ -36,21 +37,34 @@ export default function App() {
           onChangeText={goalInputHandler}
           placeholder="Enter a goal..."
           value={goal}
-          style={{ borderBottomColor: 'black', borderBottomWidth: 1, marginBottom: 2}}/>
+          style={{ 
+            borderBottomColor: 'black', 
+            borderBottomWidth: 1, 
+            marginBottom: 2}}/>
           
         </View>
         <View stlye={{flex: 1}}>
           <Button title="Click me" onPress={addGoalHandler}/>
         </View>
       </View>
-      <View>
-        {
-          goals.map(oneGoal => (
-          <Text 
-          style={{padding: 10, backgroundColor: 'red', color: 'white'}}
-          key={oneGoal}>{oneGoal}</Text>))
-        }
-      </View>
+      <FlatList
+      data={goals}
+      renderItem={({item, index}) => (
+        <View 
+        style={{
+          padding: 20, 
+          backgroundColor: '#fafafa', 
+          color: 'white',
+          borderColor: 'black',
+          borderWidth: 1,
+          marginBottom: 8
+        }}
+        key={ index.toString() }>
+          <Text style={{ fontSize: 30, marginBottom: 8}}>Title</Text>
+          <Text>{ item }</Text>
+        </View>
+      )}>
+      </FlatList>
     </View>
   );
 }
