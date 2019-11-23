d0444e410d95262aba96a48ae3ef916f6d5f15a4
daniel corcoran <danielc.92@outlook.com>
2019-11-23 15:55:14 +1100

    Added mock data dir

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import data from './mock_data';
 
 export default function App() {
   return (
---
+++ mock_data/index.js
@@ -0,0 +1,26 @@
+const humans = [
+    {
+        name: 'Daniel Smith',
+        favouriteColour: 'blue',
+        favouriteNumber: 8
+    },
+    {
+        name: 'George Smith',
+        favouriteColour: 'blue',
+        favouriteNumber: 12
+    },
+    {
+        name: 'Jane Smith',
+        favouriteColour: 'green',
+        favouriteNumber: 3
+    },
+    {
+        name: 'Susan Cairns',
+        favouriteColour: 'red',
+        favouriteNumber: 10000
+    }
+]
+
+module.exports = {
+    humans
+}
