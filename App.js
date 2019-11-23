748b6f347201bfbb86237488ce29ff1464f653b2
daniel corcoran <danielc.92@outlook.com>
2019-11-23 17:05:09 +1100

    Testing useState

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import data from './mock_data';
 
 export default function App() {
+
+  const [outputText, setOutputText] = useState('Open up App.js to start working on app.')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{ outputText }</Text>
+      <Button title="Change text" onPress={()=>setOutputText('This is the new text')}></Button>
     </View>
   );
 }
