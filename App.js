26a8084f02683d6d0980db8f709bd2e555a46e66
daniel corcoran <danielc.92@outlook.com>
2019-11-24 10:53:01 +1100

    Added startgame screen

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Header from './components/Header';
+import StartGameScreen from './screens/StartGame';
 
 function App() {
     return (
         <View style={styles.screen}>
             <Header title="Daniels Cool Beans App"/>
-
+            <StartGameScreen/>
         </View>
     )
 }
--- components/Header.js
+++ components/Header.js
@@ -13,7 +13,7 @@ const styles = StyleSheet.create({
     header: {
         width: '100%',
         height: 90,
-        paddingTop: 36,
+        paddingTop: 30,
         backgroundColor: '#f7287b',
         alignItems: 'center',
         justifyContent: 'center'
