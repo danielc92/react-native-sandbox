9edf98fc1a9022c10e31e06b1d8d62885f512e93
daniel corcoran <danielc.92@outlook.com>
2019-11-24 11:18:30 +1100

    Worked on StartGame

--- screens/StartGame.js
+++ screens/StartGame.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Button, Text, TextInput } from 'react-native';
 
 const StartGameScreen = props => {
     return (
         <View style={styles.screen}>
             <Text>The Game Screen!</Text>
+            <View style={styles.container}>
+                <Text style={{textAlign: 'center'}}>Select a number</Text>
+                <TextInput/>
+                <View style={styles.buttonContainer}>
+                    <Button title="Reset"/>
+                    <Button title="Confirm"/>
+                </View>
+            </View>
         </View>
     )
 }
@@ -12,9 +20,19 @@ const StartGameScreen = props => {
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        padding: 10,
+        padding: 15,
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    container: {
+        borderColor: 'grey',
+        borderWidth: 1,
+        padding: 10,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-evenly',
     }
 })
 export default StartGameScreen;
