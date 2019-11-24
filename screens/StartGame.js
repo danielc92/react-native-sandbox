636d1ac0d6fdee7350a7105c06fea4ddfed89495
daniel corcoran <danielc.92@outlook.com>
2019-11-24 13:31:45 +1100

    Added functions to handle number changes

--- screens/StartGame.js
+++ screens/StartGame.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, Button, Text, TextInput } from 'react-native';
 
 const StartGameScreen = props => {
+
+    const [number, setNumber] = useState(null);
+    const [input, setInput] = useState(null);
+    
+    const handleChangeNumber = (e) => {
+        setInput(e.nativeEvent.text)
+    }
+
+    const handleSetNumber = () => {
+        const parsedNumber = parseFloat(input);
+        if (parsedNumber !== NaN) {
+            setNumber(parsedNumber);
+        }
+    }
+
     return (
         <View style={styles.screen}>
             <Text>The Game Screen!</Text>
+            {
+                number ? 
+                <Text style={{ fontSize: 26, fontWeight: 'bold'}}>You have chosen {number}</Text>
+                :<Text style={{ textAlign: 'center', fontSize: 26, fontWeight: 'bold'}}>You haven't chosen a number yet!</Text>
+            }
             <View style={styles.container}>
-                <Text style={{textAlign: 'center'}}>Select a number</Text>
-                <TextInput/>
+                <Text style={{
+                    textAlign: 'center', 
+                    marginBottom: 16}}>Select a number</Text>
+                <TextInput 
+                onChange={handleChangeNumber}
+                keyboardType="numeric"
+                style={{ marginBottom: 16, borderColor: '#f2f2f2', borderWidth: 1}}/>
                 <View style={styles.buttonContainer}>
-                    <Button title="Reset"/>
-                    <Button title="Confirm"/>
+                    <Button color="#f7287b" title="Reset"/>
+                    <Button onPress={handleSetNumber} title="Confirm"/>
                 </View>
             </View>
         </View>
@@ -25,7 +50,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center'
     },
     container: {
-        borderColor: 'grey',
+        borderColor: '#f2f2f2',
         borderWidth: 1,
         padding: 10,
     },
