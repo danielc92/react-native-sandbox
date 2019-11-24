33618072eb50962bf282d1b20f15e6f2dbde7516
daniel corcoran <danielc.92@outlook.com>
2019-11-24 13:37:18 +1100

    Added reset Handler

--- components/Header.js
+++ components/Header.js
@@ -3,20 +3,24 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const Header = props => {
     return (
-        <View style={styles.header}>
-            <Text>{props.title}</Text>
+        <View style={styles.headerContainer}>
+            <Text style={styles.headerText}>{props.title}</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    header: {
+    headerContainer: {
         width: '100%',
         height: 90,
         paddingTop: 30,
         backgroundColor: '#f7287b',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+    },
+    headerText: {
+        fontSize: 24, 
+        color: '#fff',
     }
 })
 
--- screens/StartGame.js
+++ screens/StartGame.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Button, Text, TextInput } from 'react-native';
+import { View, StyleSheet, Button, Alert, Text, TextInput } from 'react-native';
 
 const StartGameScreen = props => {
 
@@ -15,6 +15,12 @@ const StartGameScreen = props => {
         if (parsedNumber !== NaN) {
             setNumber(parsedNumber);
         }
+        Alert.alert('Success', 'number has been set!')
+    }
+
+    const handleResetNumber = () => {
+        setNumber(null);
+        Alert.alert('Success', 'number has been reset!')
     }
 
     return (
@@ -22,20 +28,25 @@ const StartGameScreen = props => {
             <Text>The Game Screen!</Text>
             {
                 number ? 
-                <Text style={{ fontSize: 26, fontWeight: 'bold'}}>You have chosen {number}</Text>
-                :<Text style={{ textAlign: 'center', fontSize: 26, fontWeight: 'bold'}}>You haven't chosen a number yet!</Text>
+                <Text style={styles.chosenText}>You have chosen {number}</Text>
+                :<Text style={styles.chosenText}>You haven't chosen a number yet!</Text>
             }
             <View style={styles.container}>
-                <Text style={{
-                    textAlign: 'center', 
-                    marginBottom: 16}}>Select a number</Text>
+                <Text style={styles.numberText}>Select a number</Text>
                 <TextInput 
-                onChange={handleChangeNumber}
-                keyboardType="numeric"
-                style={{ marginBottom: 16, borderColor: '#f2f2f2', borderWidth: 1}}/>
+                    onChange={handleChangeNumber}
+                    keyboardType="numeric"
+                    style={{ marginBottom: 16, borderColor: '#f2f2f2', borderWidth: 1}}
+                />
                 <View style={styles.buttonContainer}>
-                    <Button color="#f7287b" title="Reset"/>
-                    <Button onPress={handleSetNumber} title="Confirm"/>
+                    <Button
+                        onPress={handleResetNumber} 
+                        color="#f7287b" title="Reset"
+                    />
+                    <Button
+                        onPress={handleSetNumber} 
+                        title="Confirm"
+                    />
                 </View>
             </View>
         </View>
@@ -58,6 +69,16 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         width: '100%',
         justifyContent: 'space-evenly',
+    },
+    numberText: {
+        textAlign: 'center', 
+        marginBottom: 16
+    },
+    chosenText: { 
+        textAlign: 'center', 
+        fontSize: 26, 
+        fontWeight: 'bold'
     }
+    
 })
 export default StartGameScreen;
